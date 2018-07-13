e7639d616b4d9393e91fab5c4113bf1c6208d587
christensenjoshua <christensen.joshua@outlook.com>
2018-07-12 19:18:11 -0600

    added total wins beyond the single win of 10 points

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var computerChoices = ['rock', 'paper', 'scissors']
 var resultElement = document.getElementById("gameResult")
-var playerScore = 0
-var computerScore = 0
-var playerScoreElement = document.getElementById("playerScore")
-var computerScoreElement = document.getElementById("computerScore")
+var playerScore = 0; var playerWins = 0
+var computerScore = 0; var computerWins = 0
+var playerScoreElement = document.getElementById("playerScore"); var playerWinsElement = document.getElementById("playerWins")
+var computerScoreElement = document.getElementById("computerScore"); var computerWinsElement = document.getElementById("computerWins")
 var gameResult = ''
+var clear = false
 function play(choice) {
     var thisNumber = Math.floor(Math.random() * Math.floor(3))
     var thisChoice = computerChoices[thisNumber]
@@ -51,7 +52,11 @@ function play(choice) {
     }
     updateScreen()
 }
-function playAgain(){
+function playAgain(clear){
+    if(clear){
+        playerWins = 0
+        computerWins = 0
+    }
     playerScore = 0
     computerScore = 0
     gameResult = ''
@@ -60,17 +65,23 @@ function playAgain(){
 function updateScreen(){
     playerScoreElement.innerText = playerScore
     computerScoreElement.innerText = computerScore
+    playerWinsElement.innerText = playerWins
+    computerWinsElement.innerText = computerWins
     resultElement.innerText = gameResult
         if(playerScore >= 10){
             if(computerScore == playerScore){
                 alert("Congratulations! You both win!! (kindof)")
+                playerWins++
+                computerWins++
                 playAgain()
             }else{
                 alert("Congratulations, you win!!!")
+                playerWins++
                 playAgain()
             }
         }else if(computerScore >= 10){
             alert("Sorry, your opponent bested you!")
+            computerWins ++
             playAgain()
         }
     }
